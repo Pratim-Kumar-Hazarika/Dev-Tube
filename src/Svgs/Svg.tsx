24917e4fb380095b1332e1930e95a62aff4e5dfb
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-03 01:47:00 +0530

    add icons svgs

---
+++ src/Svgs/Svg.tsx
@@ -0,0 +1,49 @@
+import { SVGProps } from "react-router/node_modules/@types/react"
+
+
+export function IconoirSunLight(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><g strokeWidth="1.5" fill="none"><path d="M12 18a6 6 0 1 0 0-12a6 6 0 0 0 0 12z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path><path d="M22 12h1" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path><path d="M12 2V1" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path><path d="M12 23v-1" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path><path d="M20 20l-1-1" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path><path d="M20 4l-1 1" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path><path d="M4 20l1-1" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path><path d="M4 4l1 1" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path><path d="M1 12h1" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path></g></svg>
+    )
+  }
+
+  
+export function MdiHistory(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M13.5 8H12v5l4.28 2.54l.72-1.21l-3.5-2.08V8M13 3a9 9 0 0 0-9 9H1l3.96 4.03L9 12H6a7 7 0 0 1 7-7a7 7 0 0 1 7 7a7 7 0 0 1-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42A8.896 8.896 0 0 0 13 21a9 9 0 0 0 9-9a9 9 0 0 0-9-9" fill="currentColor"></path></svg>
+    )
+  }
+  
+export function MdiPlaylistPlus(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M2 16h8v-2H2m16 0v-4h-2v4h-4v2h4v4h2v-4h4v-2m-8-8H2v2h12m0 2H2v2h12v-2z" fill="currentColor"></path></svg>
+    )
+  }
+
+  
+export function IcRoundThumbUp(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M13.12 2.06L7.58 7.6c-.37.37-.58.88-.58 1.41V19c0 1.1.9 2 2 2h9c.8 0 1.52-.48 1.84-1.21l3.26-7.61C23.94 10.2 22.49 8 20.34 8h-5.65l.95-4.58c.1-.5-.05-1.01-.41-1.37c-.59-.58-1.53-.58-2.11.01zM3 21c1.1 0 2-.9 2-2v-8c0-1.1-.9-2-2-2s-2 .9-2 2v8c0 1.1.9 2 2 2z" fill="currentColor"></path></svg>
+    )
+  }
+
+  
+export function IcRoundExplore(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M12 10.9c-.61 0-1.1.49-1.1 1.1s.49 1.1 1.1 1.1c.61 0 1.1-.49 1.1-1.1s-.49-1.1-1.1-1.1zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm2.19 12.19L6 18l3.81-8.19L18 6l-3.81 8.19z" fill="currentColor"></path></svg>
+    )
+  }
+
+  
+export function MdiHome(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5z" fill="currentColor"></path></svg>
+    )
+  }
+
+  
+export function MdiAlphaDBox(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M9 7v10h4a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2H9m2 2h2v6h-2V9M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z" fill="currentColor"></path></svg>
+  )
+}
