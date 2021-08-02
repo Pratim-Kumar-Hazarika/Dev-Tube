58d5a0413579892cba95b37138154daff650d225
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-03 01:46:24 +0530

    create signup screen

---
+++ src/Screens/Signup.tsx
@@ -0,0 +1,21 @@
+import "../Components/Login/Login.css"
+import SignupForm from "../Signup/SignupForm"
+
+import {ReactComponent as HeroImage} from "../Svgs/devtube2.svg"
+export default function Signup() {
+    return (
+        <div className="login">
+            <div className="login_text bg_color">
+                <div className="quote">
+                    <span className="note_quote ">Welcome to <span className="dev_text">Dev</span>Tube</span>
+                </div>
+                <div className="hero_image1">
+                    <HeroImage/>
+                </div>
+            </div>
+            <div className="login_form">
+               <SignupForm/>
+            </div>
+        </div>
+    )
+}
