656fcb75cf4adabefb6bd30514413c00f2b82fe4
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-03 01:43:01 +0530

    add : left bar component

---
+++ src/Components/LeftBar/LeftBar.tsx
@@ -0,0 +1,92 @@
+import "./LeftBar.css";
+import {Link} from "react-router-dom";
+import {IcRoundExplore, IcRoundThumbUp, MdiHistory, MdiHome, MdiPlaylistPlus} from "../../Svgs/Svg";
+export function LeftBar() {
+    return (
+        <div
+            className="left_div"
+            style={{
+            backgroundColor: "black",
+            borderBottom: "3px solid whitesmoke"
+        }}>
+            <Link to="/" style={{
+                textDecoration: "none"
+            }}>
+                <div className="leftBarDivContents">
+                    <MdiHome className=" leftbar_icons"/>
+                    <span
+                        style={{
+                        color: "white"
+                    }}
+                        className="left_bar_text">
+                        HOME
+                    </span>
+                </div>
+            </Link>
+            <Link
+                to="/genre"
+                style={{
+                textDecoration: "none"
+            }}>
+                <div className="leftBarDivContents">
+                    <IcRoundExplore className=" leftbar_icons"/>
+                    <span
+                        style={{
+                        color: "white"
+                    }}
+                        className="left_bar_text">
+                        EXPLORE
+                    </span>
+                </div>
+            </Link>
+            <Link
+                to="/likedvideo"
+                style={{
+                textDecoration: "none"
+            }}>
+                <div className="leftBarDivContents">
+                    <IcRoundThumbUp className=" leftbar_icons"/>
+                    <span
+                        style={{
+                        color: "white"
+                    }}
+                        className="left_bar_text">
+                        LIKED{" "}
+                    </span>
+                </div>
+            </Link>
+            <Link
+                to="/playlist"
+                style={{
+                textDecoration: "none"
+            }}>
+                <div className="leftBarDivContents">
+                    <MdiPlaylistPlus className=" leftbar_icons"/>
+                    <span
+                        style={{
+                        color: "white"
+                    }}
+                        className="left_bar_text">
+                        PLAYLIST
+                    </span>
+                </div>
+            </Link>
+            <Link
+                to="/historyvideo"
+                style={{
+                textDecoration: "none"
+            }}>
+                <div className="leftBarDivContents">
+                    <MdiHistory className=" leftbar_icons"/>
+                    <span
+                        style={{
+                        color: "white"
+                    }}
+                        className="left_bar_text">
+                        HISTORY
+                    </span>
+                </div>
+            </Link>
+        </div>
+    );
+}
