e4dc7bddb85c484296fd7219d833585d15c37e4e
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-03 01:46:00 +0530

    create explore screen

---
+++ src/Screens/Explore.tsx
@@ -0,0 +1,11 @@
+import Header from '../Components/Header/Header'
+import { LeftBar } from '../Components/LeftBar/LeftBar'
+
+export default function Explore() {
+    return (
+        <div>
+           <Header/>
+           <LeftBar/>
+        </div>
+    )
+}
