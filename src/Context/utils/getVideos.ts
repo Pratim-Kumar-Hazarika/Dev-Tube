5c35b51de3227c12ae83346e7b0db2ad98230732
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-03 01:45:09 +0530

    add : get videos from server

---
+++ src/Context/utils/getVideos.ts
@@ -0,0 +1,19 @@
+
+import axios from "axios";
+import { Dispatch } from "react";
+import { Video, VideosFromServer } from "../reducers/video.reducer.types";
+
+
+export function getVideosFromServer(dispatch: Dispatch<{ type: "VIDEOS_FROM_SERVER"; payload: { allVideos: Video[] } }>): void {
+    (async function () {
+        try {
+            const response = await axios.get<VideosFromServer>(`https://stormy-waters-06208.herokuapp.com/video`);
+            if (response.status === 200) {
+                dispatch({ type: "VIDEOS_FROM_SERVER", payload: { allVideos: response.data.videos } });
+            }
+            return response.data.videos;
+        } catch (error) {
+            console.log("error while getting all the videos from server");
+        }
+    })();
+}
