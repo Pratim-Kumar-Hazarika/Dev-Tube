316561d0f6816eb731aa35b1971088251be5564c
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-03 01:46:08 +0530

    create home screen

---
+++ src/Screens/Home.tsx
@@ -0,0 +1,13 @@
+import Header from '../Components/Header/Header'
+import {LeftBar} from '../Components/LeftBar/LeftBar'
+import {Row} from '../Components/Rows/Row'
+
+export default function Home() {
+    return ( 
+    <> 
+        <Header/> 
+        <LeftBar/>
+        <Row/>
+     </>
+    )
+}
