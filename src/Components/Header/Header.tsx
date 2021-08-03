4b2967d45af569f1f794c2eadadf3e02841a1964
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-04 01:19:26 +0530

    refactor : header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,34 @@
 import "./Header.css";
-import {useNavigate} from "react-router-dom";
-import {IconoirSunLight} from "../../Svgs/Svg";
+import {useLocation, useNavigate} from "react-router-dom";
+import {
+    IconoirSunLight,
+    IcRoundExplore,
+    IcRoundThumbUp,
+    MdiHistory,
+    MdiHome,
+    MdiPlaylistPlus
+} from "../../Svgs/Svg";
+import {Link} from "react-router-dom";
+import HeaderIcons from "../../Reuseable/HeaderIcons";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 export default function Header() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const state = useLocation();
+    const {videoId} = useParams();
+    const [hideIcon,setHideIcon] = useState(true)
+    
+   useEffect(() =>{
+        if(state.pathname==='/'){
+            setHideIcon(false)
+        }
+        if(state.pathname===`/video/${videoId}`){
+            setHideIcon(true)
+        }
+   },[state.pathname])
+
     return (
-        <div>
+        <div >
             <div
                 className="navbar-div"
                 style={{
@@ -17,10 +41,13 @@ export default function Header() {
                             <span className="dev_text">Dev</span>Tube</span>
                     </div>
                 </div>
-                <div className="color_modes">
+                <div className="head_icons" style={{display:hideIcon ?"":"none"}}>
+                 <HeaderIcons/>
+                </div>
+                <div className="color_modes" style={{display:hideIcon ?"none":""}}>
                     <IconoirSunLight className=" nav_icons"/>
                 </div>
-                <div>
+                <div style={{display:hideIcon ?"none":""}}>
                     <button onClick={()=>navigate("/login")} className="homebtn ">LOG IN</button>
                 </div>
             </div>
