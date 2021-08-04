7230dd80d47af72966299a4f3b82df1f686e235e
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-04 19:07:12 +0530

    create Playlist.tsx

---
+++ src/Reuseable/Playlists.tsx
@@ -0,0 +1,45 @@
+import "../CSS/Playlist.css"
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { IonMdTrash } from "../Svgs/Svg"
+import { useVideo } from "../Context/VideoProvider"
+import { deleteVideoFromPlaylist } from "../Context/utils/deleteVideoFromPlaylist"
+
+
+
+export type PlaylistProps = {
+    _id :string;
+    thumbnail:string;
+    image:string;
+    name: string;
+    views:string;
+    playlistName:string
+}
+export const Playlists: React.FC<PlaylistProps> = ({_id,thumbnail,image,name,playlistName}) => {
+    const {dispatch} = useVideo()
+   
+    return (
+        <div className="playlist_row">
+        <div style={{ height: "100%" }}>
+          <Link to={`/video/${_id}`}>
+            <img
+              className="video_image"
+              src={thumbnail}
+              alt="video_image"
+            />
+          </Link>
+          <div className="video_text">
+            <div>
+               <img src={image}  alt="Avatar" className="avatar"/>
+            </div>
+            <div  className="video_name_trash_icon" >
+              <div className="playlist_video_text">
+                {name.slice(0,25) + "..."}
+              </div>
+              <div className="trash" onClick={()=>deleteVideoFromPlaylist({playlistName,_id,dispatch})}><IonMdTrash/></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
