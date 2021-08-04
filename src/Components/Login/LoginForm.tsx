619467a387da421e4258511f9b9c839aee786da6
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-05 01:07:18 +0530

    add: setvalues for input fiels

--- src/Components/Login/LoginForm.tsx
+++ src/Components/Login/LoginForm.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./Login.css"
 import { Formik, Form, Field } from "formik";
 import { validateEmail, validatePassword } from '../../Context/utils/Validations';
+import { useAuth } from '../../Context/AuthProvider';
 interface MyFormValues {
     password: string;
     email: string;
   }
 export default function LoginForm() {
     const initialValues: MyFormValues = { password: "", email: "" };
+    const {email,setEmail,password,setPassword,loginUserWithCredentials} = useAuth()
+
     return (<> 
     <div className="login_content">
         <div className="login_heading">
@@ -27,19 +30,19 @@ export default function LoginForm() {
           <Form>
             <div className="email_div">
             <div>Email</div>
-              <Field name="email" validate={(value:string)=>validateEmail(value)} className="password_input"/>
+              <Field name="email" validate={(value:string)=>(validateEmail({value,setEmail}))} className="password_input"  />
               {errors.email && touched.email && <div className="error_text">{errors.email}</div>}
             </div>
             
             <div className="password_div">
             <div>Password</div>
-              <Field name="password" validate={(value:string)=>validatePassword(value)} className="password_input" type="password"/>
+              <Field name="password" validate={(value:string)=>validatePassword({value,setPassword})} className="password_input" type="password"/>
               {errors.password && touched.password && (
                 <div className="error_text">{errors.password}</div>
               )}
             </div>
             <div>
-            <button type="submit" className="login_btn">Submit</button>
+            <button type="submit" className="login_btn" onClick={()=>loginUserWithCredentials(email,password)}>Submit</button>
             <div className="dont_have_account_div">
                 <span>Don't have an account ?</span>
                 <Link to="/signup">
