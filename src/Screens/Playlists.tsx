c5d25488bf77230593a2881a2c9d5a74d44ae9e3
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-04 19:07:24 +0530

    create playlists

---
+++ src/Screens/Playlists.tsx
@@ -0,0 +1,16 @@
+import "../CSS/Playlist.css"
+import { LeftBar } from "../Components/LeftBar/LeftBar";
+import Header from "../Components/Header/Header";
+import { PlaylistlistRow } from "../Components/PlaylistRow/PlaylistRow";
+
+export function PlayLists() {
+  return (
+      <>
+       <Header />
+        <div style={{marginTop:"5rem"}}>
+        <LeftBar />
+         </div>
+        <PlaylistlistRow/>
+    </>
+  );
+}
