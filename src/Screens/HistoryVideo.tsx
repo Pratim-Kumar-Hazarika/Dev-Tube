9d05067a98e9c5ad28322a4564d7c0833bdd8570
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-04 14:18:14 +0530

    create history video screen

---
+++ src/Screens/HistoryVideo.tsx
@@ -0,0 +1,27 @@
+import "../CSS/LikedHistoryVideo.css"
+import { useVideo } from "../Context/VideoProvider";
+import { LeftBar } from "../Components/LeftBar/LeftBar";
+import Header from "../Components/Header/Header";
+import { Thumbnail } from "../Components/Thumbnail/Thumbnail";
+export function HistoryVideo() {
+const {state} = useVideo()
+  return (
+      <>
+      <Header/>
+      <LeftBar/>
+        <div className="main">
+        <div className="like_history_video">
+            <div className="parent_video_div">
+            <div className="text">History Videos</div>
+            {state.historyVideos.map((item) => {
+                const { _id, thumbnail, name, artist } = item;
+                return (
+                <Thumbnail name={name} artist={artist} thumbnail={thumbnail} _id={_id}/>
+                );
+            })}
+            </div>
+        </div>
+        </div>
+    </>
+  );
+}
