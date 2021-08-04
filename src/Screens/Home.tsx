158749b651f6c349ef54c55ed495046c63ac0bb7
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-04 14:18:25 +0530

    fix : css

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -3,11 +3,13 @@ import {LeftBar} from '../Components/LeftBar/LeftBar'
 import {Row} from '../Components/Rows/Row'
 
 export default function Home() {
-    return ( 
-    <> 
-        <Header/> 
-        <LeftBar/>
-        <Row/>
-     </>
-    )
+return (
+<>
+    <Header />
+    <div style={{marginTop:"5rem"}}>
+        <LeftBar />
+    </div>
+    <Row />
+</>
+)
 }
