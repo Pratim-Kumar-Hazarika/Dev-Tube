63053a6c852bf5b7ddfae7fc0ca80085d15f2096
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:12:31 +0530

    update : add server call

--- src/Context/utils/addVideoToPlaylist.ts
+++ src/Context/utils/addVideoToPlaylist.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Dispatch } from 'react';
 import { ACTION } from '../reducers/video.reducer';
 import { ReducerInitialState } from './../reducers/video.reducer.types';
@@ -6,13 +7,29 @@ export type AddToPlaylist = {
     playlistName :string;
     state : ReducerInitialState;
     _id:string;
-    dispatch:Dispatch<ACTION>
+    dispatch:Dispatch<ACTION>,
+    userID:string;
+    token:string
 }
 
-export async function addVideoToPlaylist( {playlistName,state,_id,dispatch} :AddToPlaylist){
+export async function addVideoToPlaylist( {playlistName,state,_id,dispatch,userID,token} :AddToPlaylist){
     const getPlaylist = state.playlists.filter((playlist)=>playlist.name === playlistName)
     const checkVideoIfInPlaylist = getPlaylist[0].video.filter((video)=>video._id === _id)
     if(checkVideoIfInPlaylist.length <1){
-     return dispatch({type:"ADD_VIDEO_TO_PLAYLIST",payload:{playlistName:playlistName,videoId:_id}})        
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_LOCAL_HOST}/user/${userID}/playlists/video`,{
+                name:playlistName,
+                videoId:_id
+            },{
+                headers:{
+                    authorization:token
+                }
+            })
+            if(response.status === 200){
+            dispatch({type:"ADD_VIDEO_TO_PLAYLIST",payload:{playlistName:playlistName,videoId:_id}})
+            }
+        } catch (error) {
+           console.log("video not added to playlist") 
+        }    
     } 
   }
