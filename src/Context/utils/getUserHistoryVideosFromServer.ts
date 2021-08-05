9e877caf3f9cef083c0a719ac5ad5d85cc93c369
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:10:23 +0530

    create : get user history from server

---
+++ src/Context/utils/getUserHistoryVideosFromServer.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { Dispatch } from "react";
+import { ACTION } from "../reducers/video.reducer";
+
+
+export type GetHistoryVideosFromServer = {
+    dispatch :Dispatch<ACTION>;
+    token:string;
+    userID:string
+}
+export function getUserHistoryVideosFromServer({dispatch,token,userID}:GetHistoryVideosFromServer): void {
+    (async function () {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_LOCAL_HOST}/user/${userID}/history/video`,{
+                headers:{
+                    authorization:token
+                }
+            });
+            if (response.status === 200) {
+                dispatch({ type: "HISTORY_VIDEOS_FROM_SERVER", payload: { historyVideos:response.data.getUserVideos.historyVideos} });            
+            }
+            return response.data.videos;
+        } catch (error) {
+            console.log("error while getting all the videos from server");
+        }
+    })();
+}
