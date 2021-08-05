b9aa135c21e21e1b1a0e41c399f0122abc0d9038
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:11:07 +0530

    update : add backend

--- src/Context/utils/deleteVideoFromPlaylist.ts
+++ src/Context/utils/deleteVideoFromPlaylist.ts
@@ -1,12 +1,29 @@
+import axios from "axios"
 import { Dispatch } from "react"
 import { ACTION } from "../reducers/video.reducer"
 
 export type DeleteVideoFromPlaylist = {
     playlistName :string;
     _id:string;
-    dispatch:Dispatch<ACTION>
+    dispatch:Dispatch<ACTION>;
+    userID:string;
+    token:string;
 }
-export function deleteVideoFromPlaylist({playlistName,_id,dispatch}:DeleteVideoFromPlaylist){
-        
-    dispatch({type:"DELETE_VIDEO_FROM_PLAYLIST",payload:{playlistName:playlistName,videoId:_id}})
+export async function deleteVideoFromPlaylist({playlistName,_id,dispatch,userID,token}:DeleteVideoFromPlaylist){
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_LOCAL_HOST}/user/${userID}/playlists/video/delete`,{
+                name:playlistName,
+                videoId:_id
+            },{
+                headers:{
+                    authorization:token
+                }
+            })
+            if(response.status === 200){
+                dispatch({type:"DELETE_VIDEO_FROM_PLAYLIST",payload:{playlistName:playlistName,videoId:_id}})
+            }
+        } catch (error) {
+            console.log("video not deleted from playlist")
+        }
+  
 }
