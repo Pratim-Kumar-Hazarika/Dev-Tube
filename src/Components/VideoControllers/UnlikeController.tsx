5c74ceb7fcb3016b3e2709960daf2d6a5334781c
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:13:17 +0530

    update : pass userID,token

--- src/Components/VideoControllers/UnlikeController.tsx
+++ src/Components/VideoControllers/UnlikeController.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useAuth } from '../../Context/AuthProvider'
 import { dislikeClickHandler } from '../../Context/utils/dislikeClickHandler'
 import { useVideo } from '../../Context/VideoProvider'
 import { MdiThumbDown } from '../../Svgs/Svg'
 
 export const UnlikeController : React.FC < {_id:string} > = ({_id}) => {
     const {dispatch,dislikedVideo} = useVideo()
+    const {userID,token} = useAuth()
     return (
-        <div className="like_items" onClick={() => dislikeClickHandler(_id, dispatch)}>
+        <div className="like_items" onClick={() => dislikeClickHandler({_id,dispatch,userID,token})}>
             <MdiThumbDown
                 className="iconify playvideoIcons"
                 style={{
