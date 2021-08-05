cbb103fe60e51688a3a95a0ae81d88a7d46bb3b0
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:13:39 +0530

    create topic.tsx

---
+++ src/Components/Topic/Topic.tsx
@@ -0,0 +1,51 @@
+import "./Topic.css";
+import { Link } from "react-router-dom";
+
+export type Topic = {
+    url:string;
+    name:string;
+    artist:string;
+    views:string;
+    image:string;
+    thumbnail:string;
+    id:string;
+}
+
+export default function Topic({
+  url,
+  name,
+  artist,
+  views,
+  image,
+  thumbnail,
+  id
+}:Topic)  {
+  return (
+    <div className="ds_mainDiv">
+      <div className="topic_video_contents" >
+        <div className="topic_thumbnail_div">
+          <Link to={{ pathname: `/video/${id}` }}>
+            <img alt="artist"  src={thumbnail} className="topic_thumnail_" />
+          </Link>
+        </div>
+        <div className="topic_titles_views_avatar" >
+        <div className="ds_avatar">
+        <img src={image}  alt="Avatar" className="avatar ds_avatar_img"/>
+            <span className="ds_artistname">{artist} ♪</span>
+          </div>
+          <div className="ds_title_views">
+            <div className="ds_name_div">
+              <span className="ds_name">{name}</span>
+            </div>
+            <div className="topic_views_div">
+              <span className="ds_views">{views}</span>
+            </div>
+            <div className="topic_artist_div">
+              <span className="ds_artist">{artist} ♪</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
