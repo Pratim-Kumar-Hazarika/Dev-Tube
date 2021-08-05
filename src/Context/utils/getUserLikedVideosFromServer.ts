19c7258aa832d50d068f5bfdfb04a6665dff3523
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:10:09 +0530

    create : get user liked videos from server

---
+++ src/Context/utils/getUserLikedVideosFromServer.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { Dispatch } from "react";
+import { ACTION } from "../reducers/video.reducer";
+
+
+export type GetLikedVideosFromServer = {
+    dispatch :Dispatch<ACTION>;
+    token:string;
+    userID:string
+}
+export function getUserLikedVideosFromServer({dispatch,token,userID}:GetLikedVideosFromServer): void {
+    (async function () {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_LOCAL_HOST}/user/${userID}/liked/video`,{
+                headers:{
+                    authorization:token
+                }
+            });
+          
+            if (response.status === 200) {
+                dispatch({ type: "LIKED_VIDEOS_FROM_SERVER", payload: { likedVideos:response.data.getUserVideos.likedVideos} });            
+            }
+            return response.data.videos;
+        } catch (error) {
+            console.log("error while getting all the videos from server");
+        }
+    })();
+}
