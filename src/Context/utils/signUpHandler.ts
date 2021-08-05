6919550da279ea493ef6840b71e8206bf2df1223
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:07:54 +0530

    update : add server

--- src/Context/utils/signUpHandler.ts
+++ src/Context/utils/signUpHandler.ts
@@ -1,11 +1,32 @@
+import { Navigate } from 'react-router-dom';
+import axios from "axios"
+import { NavigateFunction } from 'react-router';
+
 export type SignupHandler ={
     name :string;
     email:string;
-    password:string
+    password:string;
+    navigate:NavigateFunction
 }
-export async function signUpHandler({name,email,password}:SignupHandler){
-    console.log("name",name)
-    console.log("email",email)
-    console.log("passowrd",password)
+export type PostData = {
+    name :string;
+    password:string;
+    email:string
+}
+
+export async function signUpHandler({name,email,password,navigate}:SignupHandler){
+    const data :PostData = {
+        name :name,
+        email:email,
+        password:password
+    }
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_LOCAL_HOST}/user`,data)
+       if(response.status === 200){
+           navigate("/login")
+       }
+    } catch (error) {
+        console.log("Error while adding user to db",error)
+    }
 
 }
