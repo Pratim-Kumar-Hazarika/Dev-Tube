0516dbb679fd31839806656268c5859186e1b535
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:05:39 +0530

    update : add hero image

--- src/Screens/LikeVideos.tsx
+++ src/Screens/LikeVideos.tsx
@@ -3,23 +3,31 @@ import { useVideo } from "../Context/VideoProvider";
 import { LeftBar } from "../Components/LeftBar/LeftBar";
 import Header from "../Components/Header/Header";
 import { Thumbnail } from "../Components/Thumbnail/Thumbnail";
+import {ReactComponent as HeroImage} from "../Svgs/liked.svg"
 export function LikedVideo() {
 const {state} = useVideo()
   return (
       <>
       <Header/>
       <LeftBar/>
+      
     <div className="main">
       <div className="like_history_video">
         <div className="parent_video_div">
-        <div className="text">Liked Videos</div>
+        <div className="text" >Your Liked Videos List {state.likedVideos.length <1 && <span >is Empty</span>}</div>
           {state.likedVideos.map((item) => {
             const { _id, thumbnail, name, artist } = item;
-            return (
+            return (<>
               <Thumbnail name={name} artist={artist} thumbnail={thumbnail} _id={_id}/>
+              </>
             );
           })}
         </div>
+        {
+          state.likedVideos.length <1 &&   <div className="liked_hero_image_div">    
+          <HeroImage className="liked_hero_svg"/>
+          </div>
+        }
       </div>
     </div>
     </>
