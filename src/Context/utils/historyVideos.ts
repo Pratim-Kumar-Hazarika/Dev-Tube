1e75a5f2662000068869035a6430f4b93848cfab
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:09:03 +0530

    update : add backend + store video in db

--- src/Context/utils/historyVideos.ts
+++ src/Context/utils/historyVideos.ts
@@ -1,6 +1,27 @@
+import axios from "axios";
 import { Dispatch } from "react";
+import { ACTION } from "../reducers/video.reducer";
 
-export async function historyVideoHandler(_id:string,dispatch:Dispatch<{type:"HISTORY_VIDEOS";payload:{videoId:string}}>){
-    dispatch({type:"HISTORY_VIDEOS",payload:{videoId:_id}})
-    
+export type HistoryHandler = {
+  _id:string;
+  dispatch:Dispatch<ACTION>;
+  userID:string;
+  token:string;
 }
+
+export  async function historyVideoHandler({_id,dispatch,userID,token}:HistoryHandler) {
+      try {
+        const response = await axios.post(`${process.env.REACT_APP_LOCAL_HOST}/user/${userID}/history/video`,{
+         _id
+        },{
+          headers:{
+            authorization:token
+          }
+        })
+        if(response.status === 200){
+          dispatch({type:"HISTORY_VIDEOS",payload:{videoId :_id}})
+        }
+      } catch (error) {
+        console.log("error occured while adding to history videos")
+      }
+  }
