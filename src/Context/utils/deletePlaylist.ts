caa868daae9b89ca3ebb8cdd4a9be6280ea7f5e4
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:11:58 +0530

    create delete playlist + add backend

---
+++ src/Context/utils/deletePlaylist.ts
@@ -0,0 +1,27 @@
+import { ACTION } from './../reducers/video.reducer';
+import { Dispatch } from 'react';
+import axios from 'axios';
+export type DeletePlaylist = {
+    name:string;
+    dispatch :Dispatch<ACTION>;
+    userID:string;
+    token:string;
+}
+
+export async function deletePlaylistClickHandler({name,dispatch,userID,token}:DeletePlaylist){
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_LOCAL_HOST}/user/${userID}/playlists/delete`,{
+            name:name
+        },{
+            headers:{
+                authorization:token
+            }
+        })
+        if(response.status === 200){
+            dispatch({type:"DELETE_ENTIRE_PLAYLIST",payload:{playlistName:name}});
+        }
+    } catch (error) {
+        console.log("Error occured while deleting the playlist")
+    }
+    
+  }
