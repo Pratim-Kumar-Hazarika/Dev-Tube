f7566052835d1d932ba202b6d131d2a152e07e0e
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:10:57 +0530

    refactor : create seperate functions

--- src/Context/utils/deletVideoClickHandler.ts
+++ src/Context/utils/deletVideoClickHandler.ts
@@ -1,18 +1,23 @@
 import { Location, State } from "history"
 import { Dispatch } from "react"
 import { ACTION } from "../reducers/video.reducer"
+import { deleteVideoFromHistory } from "./deleteVideoFromHistory"
+import { deleteVideoFromLiked } from "./deleteVideoFromLiked"
 
 export type DeleteVideo= {
     _id:string;
     state:Location<State>;
-    dispatch:Dispatch<ACTION>
+    dispatch:Dispatch<ACTION>;
+    userID:string;
+    token:string;
 }
 
-export function deleteVideoClickHandler({_id,state,dispatch}:DeleteVideo){
+
+export function deleteVideoClickHandler({_id,state,dispatch,userID,token}:DeleteVideo){
     if(state.pathname=== "/history"){
-        dispatch({type:"DELETE_HISTORY_VIDEO",payload:{videoId:_id}})
+        deleteVideoFromHistory({_id,dispatch,userID,token})
     }
     if(state.pathname=== "/liked"){
-        dispatch({type:"DELETE_LIKED_VIDEO",payload:{videoId:_id}})
+        deleteVideoFromLiked({_id,dispatch,userID,token})
     }
 }
