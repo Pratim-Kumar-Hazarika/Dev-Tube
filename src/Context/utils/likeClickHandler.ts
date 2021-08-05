444d7975338d7a850a8786d9ae2cd13694fc51fe
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:08:44 +0530

    update : add backend + store video in db

--- src/Context/utils/likeClickHandler.ts
+++ src/Context/utils/likeClickHandler.ts
@@ -1,7 +1,27 @@
+import axios from "axios";
 import { Dispatch } from "react";
+import { ACTION } from "../reducers/video.reducer";
 
-export  async function likeClickHandler(_id:string,dispatch: Dispatch<{ type: "LIKE_CLICK_HANDLER"; payload:{videoId :string} }>) {
-   
-    dispatch({type:"LIKE_CLICK_HANDLER",payload:{videoId :_id}})
-    
+export type LikeClickHandler = {
+  _id:string;
+  dispatch:Dispatch<ACTION>;
+  userID:string;
+  token:string;
+}
+
+export  async function likeClickHandler({_id,dispatch,userID,token}:LikeClickHandler) {
+      try {
+        const response = await axios.post(`${process.env.REACT_APP_LOCAL_HOST}/user/${userID}/liked/video`,{
+         _id
+        },{
+          headers:{
+            authorization:token
+          }
+        })
+        if(response.status === 200){
+          dispatch({type:"LIKE_CLICK_HANDLER",payload:{videoId :_id}})
+        }
+      } catch (error) {
+        console.log("error occured while adding to like videos")
+      }
   }
