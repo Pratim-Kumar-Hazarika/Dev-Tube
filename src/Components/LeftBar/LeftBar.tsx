05b38fc129626be241172a042651080ec6a9f13c
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:02:18 +0530

    fix : route name

--- src/Components/LeftBar/LeftBar.tsx
+++ src/Components/LeftBar/LeftBar.tsx
@@ -24,7 +24,7 @@ export function LeftBar() {
                 </div>
             </Link>
             <Link
-                to="/genre"
+                to="/explore"
                 style={{
                 textDecoration: "none"
             }}>
