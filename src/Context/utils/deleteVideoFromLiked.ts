564029a5ff1b0514a5cda2825e074b28805d65d3
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:11:29 +0530

    create : delete video from likedVideos + add backend

---
+++ src/Context/utils/deleteVideoFromLiked.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { Dispatch } from "react";
+import { ACTION } from "../reducers/video.reducer";
+
+export type DeleteVideoFromLiked = {
+    _id:string;
+    dispatch :Dispatch<ACTION>;
+    userID:string;
+    token:string;
+}
+export async function deleteVideoFromLiked({_id,dispatch,userID,token}:DeleteVideoFromLiked){
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_LOCAL_HOST}/user/${userID}/liked/video/delete`,{
+            _id:_id
+        },{
+            headers:{
+                authorization:token
+            }
+        })
+        if(response.status === 200){
+            dispatch({type:"DELETE_LIKED_VIDEO",payload:{videoId:_id}});
+        }
+
+    } catch (error) {
+       console.log("video not deleted from liked") 
+    }
+}
