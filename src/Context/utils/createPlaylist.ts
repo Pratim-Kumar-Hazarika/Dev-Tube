c4a5fcd97d868cf3dd9b90283cb3717913357861
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:12:19 +0530

    update : add server call

--- src/Context/utils/createPlaylist.ts
+++ src/Context/utils/createPlaylist.ts
@@ -1,14 +1,31 @@
+import axios from 'axios';
 import { Dispatch ,SetStateAction} from 'react';
 
 type CreatePlaylist = {
     dispatch :Dispatch<{type:"ADD_PLAYLIST";payload:{playlistName:string}}>;
     playlistInput:string;
-    setPlaylistInput:Dispatch<SetStateAction<string>>
+    setPlaylistInput:Dispatch<SetStateAction<string>>;
+    userID:string;
+    token:string
 }
 
-export async function createPlaylistClickHandler({dispatch,playlistInput,setPlaylistInput}:CreatePlaylist){
+export async function createPlaylistClickHandler({dispatch,playlistInput,setPlaylistInput,userID,token}:CreatePlaylist){
     if(playlistInput !== ""){
-      dispatch({type:"ADD_PLAYLIST",payload:{playlistName:playlistInput}})
-      setPlaylistInput('')
+      try {
+        const response = await axios.post(`${process.env.REACT_APP_LOCAL_HOST}/user/${userID}/playlists`,{
+          name :playlistInput
+        },
+        {
+            headers:{
+              authorization:token
+            }
+          })
+        if(response.status === 200){
+          dispatch({type:"ADD_PLAYLIST",payload:{playlistName:playlistInput}})
+          setPlaylistInput('')
+        }
+      } catch (error) {
+        console.log("Playlist not created")
+      }
     }
   }
