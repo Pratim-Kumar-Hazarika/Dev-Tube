44221ea4640b8d32c10647d85238192639c0991d
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-06 00:10:42 +0530

    update : with types + network call

--- src/Context/utils/dislikeClickHandler.ts
+++ src/Context/utils/dislikeClickHandler.ts
@@ -1,6 +1,16 @@
+import { Location, State } from 'history';
 import { Dispatch } from 'react';
+import { ACTION } from '../reducers/video.reducer';
+import { deleteVideoFromLiked } from './deleteVideoFromLiked';
 
-export async function dislikeClickHandler(_id:string,dispatch:Dispatch<{type:"DISLIKE_CLICK_HANDLER";payload:{videoId:string}}>){
+export type DeleteVideo= {
+    _id:string;
+    dispatch:Dispatch<ACTION>;
+    userID:string;
+    token:string;
+}
+
+export async function dislikeClickHandler({_id,dispatch,userID,token}:DeleteVideo){
     dispatch({type:"DISLIKE_CLICK_HANDLER",payload:{videoId:_id}})
-     
+    deleteVideoFromLiked({_id,dispatch,userID,token})  
 }
